303a66c05bb8fa592bc518b4e8aa932eeaebfa34
TomassDiaz <tdiaz.concha@hotmail.com>
2026-09-14 17:32:03 -0300

    eliminar artefactos dist/ del repositorio e ignorar en .gitignore

--- dist/modules/trilla/trilla.dto.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// src/modules/trilla/trilla.dto.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=trilla.dto.js.map
--- dist/modules/ventas/ventas.dto.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// src/modules/ventas/ventas.dto.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=ventas.dto.js.map
